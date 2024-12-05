cfab5f15eac64a23986a5af1d9beeec9db9895ab
safurutani <bluesara24@gmail.com>
2024-12-04 23:34:30 -0800

    set default context to empty string

--- app/AppContext.js
+++ app/AppContext.js
@@ -5,11 +5,11 @@ import React, {createContext, useContext, useState} from 'react';
 const AppContext = createContext();
 
 export const AppProvider = ({children}) => {
-    const [albumId, setAlbumId] = useState(null);
-    const [artist, setArtist] = useState(null);
-    const [albumName, setAlbumName] = useState(null);
-    const [albumArt, setAlbumArt] = useState(null);
-    const [userId, setUserId] = useState(null);
+    const [albumId, setAlbumId] = useState("");
+    const [artist, setArtist] = useState("");
+    const [albumName, setAlbumName] = useState("");
+    const [albumArt, setAlbumArt] = useState("");
+    const [userId, setUserId] = useState("");
     return (
         <AppContext.Provider value= {{albumId, setAlbumId, artist, setArtist, albumName, setAlbumName, albumArt, setAlbumArt, userId, setUserId}} >
             {children}
